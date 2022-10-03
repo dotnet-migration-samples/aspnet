bf4e813f3f980ed2bbfb348c535dc43636a074e9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-10-03 15:12:12 -0400

    adding mvc w ind auth

---
+++ from-templates/mvc/MvcIndAuth/MvcIndAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcIndAuth.MvcApplication" Language="C#" %>
---
+++ from-templates/mvc/MvcIndAuth/MvcIndAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcIndAuth.Startup))]
+namespace MvcIndAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
