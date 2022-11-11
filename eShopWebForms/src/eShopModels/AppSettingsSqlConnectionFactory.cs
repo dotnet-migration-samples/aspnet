1a08e171a2dd434faec0a1b95ee1c764b07f21ce
Taylor Southwick <tasou@microsoft.com>
2022-11-11 10:07:37 -0800

    Update some prerelease packages on WebForms sample

---
+++ eShopWebForms/src/eShopModels/AppSettingsSqlConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace eShopWebForms
+{
+    public class AppSettingsSqlConnectionFactory : ISqlConnectionFactory
+    {
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings["CatalogDBContext"].ConnectionString
+            };
+        }
+    }
+}
